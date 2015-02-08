c274fd5dff782c21f6d07d7e3ac8cdfc47e59208
Lukasz <lukasz@c-call.eu>
2015-02-08 18:29:08 +0100

    version bump to 1.0.0, added file docblocks everywhere

--- src/Currency.php
+++ src/Currency.php
@@ -1,5 +1,21 @@
 <?php namespace Keios\MoneyRight;
 
+/**
+ * This file is part of the arbitrary precision arithmetic-based
+ * money value object Keios\MoneyRight package. Keios\MoneyRight
+ * is heavily inspired by Mathias Verroes' Money library and was
+ * designed to be a drop-in replacement (only some use statement
+ * tweaking required) for mentioned library. Public APIs are
+ * identical, functionality is extended with additional methods
+ * and parameters.
+ *
+ *
+ * Copyright (c) 2015 Łukasz Biały
+ *
+ * For the full copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+
 use Serializable;
 use JsonSerializable;
 use Keios\MoneyRight\Exceptions\UnknownCurrencyException;
